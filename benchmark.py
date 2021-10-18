e0dc8025dade6199ee4f3283c52594e806aef90a
Phil Eaton <phil@eatonphil.com>
2021-10-18 14:53:52 +0000

    Add sort all

--- benchmark.py
+++ benchmark.py
@@ -23,25 +23,28 @@ with open('array_of_arrays.json') as f:
             break
         except:
             pass
-    s = bench_avg(lambda: sum([row[first_int_column_index] for row in data]), 100, 'Sum int field')
+    s = bench_avg(lambda: sum([row[first_int_column_index] for row in data]), 5, 'Sum int field')
     print(s)
-    bench_avg(lambda: sorted(data, key=lambda r: r[0])[:100], 100, 'Sort by first field and take first 100')
+    bench_avg(lambda: sorted(data, key=lambda r: r[0])[:100], 5, 'Sort by first field and take first 100')
+    bench_avg(lambda: sorted(data, key=lambda r: r[0]), 5, 'Sort by first field')
 
 print('\n\nArray of objects testing\n')
 
 with open('array_of_objects.json') as f:
     data = bench_avg(lambda: json.load(f), 1, 'Read JSON')
-    s = bench_avg(lambda: sum([row[key] for row in data]), 100, 'Sum int field')
+    s = bench_avg(lambda: sum([row[key] for row in data]), 5, 'Sum int field')
     print(s)
-    bench_avg(lambda: sorted(data, key=lambda r: r[header[0]])[:100], 100, 'Sort by first field and take first 100')
+    bench_avg(lambda: sorted(data, key=lambda r: r[header[0]])[:100], 5, 'Sort by first field and take first 100')
+    bench_avg(lambda: sorted(data, key=lambda r: r[header[0]]), 5, 'Sort by first field')
 
 print('\n\nColumnar testing\n')
 
 with open('columnar.json') as f:
     data = bench_avg(lambda: json.load(f), 1, 'Read JSON')
-    s = bench_avg(lambda: sum(data[first_int_column_index][1:]), 100, 'Sum int field')
+    s = bench_avg(lambda: sum(data[first_int_column_index][1:]), 5, 'Sum int field')
     print(s)
-    def data_sort():
+    def data_sort(n=None):
         guide = sorted(range(len(data[0][1:])), key=lambda i: data[0][i+1])
-        return [row[:1] + [row[i+1] for i in guide[:100]] for row in data]
-    bench_avg(data_sort, 100, 'Sort by first field and take first 100')
+        return [row[:1] + [row[i+1] for i in (guide[:n] if n else guide)] for row in data]
+    bench_avg(lambda: data_sort(100), 5, 'Sort by first field and take first 100')
+    bench_avg(data_sort, 5, 'Sort by first field')
